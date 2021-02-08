85f6f544da1c5ce00a14c90e841542c60e64b5b9
Sal Hamidi <hamidisa87@gmail.com>
2021-02-08 08:56:16 -0500

    Add implicit vs explicit function jsx

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,30 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react'
+import ReactDom from 'react-dom'
 
-function Greeting() {
-  return <h4>this is Salem and this is my first component</h4>;
+//always return jsx
+//stateless function
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  )
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+let Book = () => {
+  return (
+    <article>
+      <Image></Image>
+    </article>
+  )
+}
+
+let Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg'
+    alt=''
+  />
+)
+
+ReactDom.render(<BookList />, document.getElementById('root'))
