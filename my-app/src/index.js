b7ccc93df3e784ca0d2a92fdecae94f0f9cf7b07
Sal Hamidi <hamidisa87@gmail.com>
2021-02-09 17:23:14 -0500

    Add books

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,35 +5,44 @@ import './index.css'
 //always return jsx
 //stateless function
 
+let book =[
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    img:'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg'},
+  {
+    title: 'The Duke and I (Bridgertons Book 1)',
+    author: 'Julia Quin',
+    img:'https://m.media-amazon.com/images/I/91SnTS+BWSL.jpg'},
+  {
+    title: 'Harry Potter and the Deathly Hallows',
+    author: 'J.K. Rowling',
+    img:'https://m.media-amazon.com/images/I/81NEMbJJg8L._SX140_.jpg'
+  }
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      
     </section>
   )
 }
 
-let Book = () => {
+let Book = ({title, author, img}) => {
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+      {img}
+      {title}
+      {author}
     </article>
   )
 }
 
 let Image = () => (
   <img
-    src='https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg'
+    src=
     alt=''
   />
 )
-
-let Title = () => <h1>Atomic Habits</h1>
-let Author = () => <h4>James Clear</h4>
 
 ReactDom.render(<BookList />, document.getElementById('root'))
