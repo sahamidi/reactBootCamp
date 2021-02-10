1c04bbb51c8af6712a2a02eca88941e1b23bb6e9
Sal Hamidi <hamidisa87@gmail.com>
2021-02-09 23:56:09 -0500

    Export components

---
+++ my-app/src/books.js
@@ -0,0 +1,22 @@
+export let books = [
+  {
+    id: 1,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Intelligent Investor',
+    author: 'Benjamin Graham ',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/91yj3mbz4JL._AC_UL200_SR200,200_.jpg',
+  },
+  {
+    id: 3,
+    title: 'Harry Potter and the Deathly Hallows',
+    author: 'J.K. Rowling',
+    img: 'https://m.media-amazon.com/images/I/81NEMbJJg8L._SX140_.jpg',
+  },
+]
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,31 +2,11 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
+import { books } from './books'
+
+import Book from './book'
 //always return jsx
 //stateless function
-
-let books = [
-  {
-    id: 1,
-    title: 'Atomic Habits',
-    author: 'James Clear',
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg',
-  },
-  {
-    id: 2,
-    title: 'The Intelligent Investor',
-    author: 'Benjamin Graham ',
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/91yj3mbz4JL._AC_UL200_SR200,200_.jpg',
-  },
-  {
-    id: 3,
-    title: 'Harry Potter and the Deathly Hallows',
-    author: 'J.K. Rowling',
-    img: 'https://m.media-amazon.com/images/I/81NEMbJJg8L._SX140_.jpg',
-  },
-]
 
 function BookList() {
   return (
@@ -35,36 +15,6 @@ function BookList() {
         return <Book key={book.id} {...book}></Book>
       })}
     </section>
-  )
-}
-
-let Book = ({ img, title, author }) => {
-  //attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = () => {
-    alert('hello world')
-  }
-  let complexExample = (author) => {
-    console.log(author)
-  }
-
-  return (
-    <article
-      className='book'
-      onMouseOver={() => {
-        console.log(title)
-      }}
-    >
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>
-        reference example
-      </button>
-      <button type='button' onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
   )
 }
 
