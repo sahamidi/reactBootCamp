2004e7e34e6658d524cbb858f3aac8702176220d
Sal Hamidi <hamidisa87@gmail.com>
2021-02-09 19:50:43 -0500

    Add button for books

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -39,11 +39,26 @@ function BookList() {
 }
 
 let Book = ({ img, title, author }) => {
+  //attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert('hello world')
+  }
+  let complexExample = (author) => {
+    console.log(author)
+  }
+
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   )
 }
